fb936c077ed1c2bb9e1b8d16c4d9a28dbaeb6a22
katleiahramos <katleiahramos@gmail.com>
2018-09-26 11:34:01 -0500

    add materailize

---
+++ bark-park/dogparks_data.md
@@ -0,0 +1,8 @@
+Wriggly Field Dog Friendly Area
+2645 N Sheffield Ave, Chicago, IL 60614
+
+Belmont Harbor Dog Beach
+N Lake Shore Dr & Lake Shore Drive Dr, Chicago, IL 60657
+
+Logan Square Dog Park
+2526 N Western Ave, Chicago, IL 60647
--- bark-park/src/App.js
+++ bark-park/src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Navbar, NavItem, Col, Card, Row} from 'react-materialize';
+
+// TODO: NavBar, logo only correct aligned when using left
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navbar brand='Bark Park' left>
+          <NavItem onClick={() => console.log('test click')}>Getting started</NavItem>
+        </Navbar>
+        <Row>
+          <Col s={3}>
+            <Card title="Wriggly Field Dog Friendly Area">
+              Count: 1
+            </Card>
+        </Col>
+        <Col s={3}>
+          <Card title="Belmont Harbor Dog Beach">
+              Count: 1
+          </Card>
+        </Col>
+        <Col s={3}>
+          <Card title="Logan Square Dog Park">
+              Count: 1
+            </Card>
+        </Col>
+      </Row>
       </div>
     );
   }
