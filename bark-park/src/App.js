ce260bb6e8b2099ac9d60cdecf58f40da6a1f2ca
katleiahramos <katleiahramos@gmail.com>
2018-09-26 11:56:07 -0500

    add state to app

--- bark-park/src/App.js
+++ bark-park/src/App.js
@@ -1,31 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Navbar, NavItem, Col, Card, Row} from 'react-materialize';
+import {Navbar, NavItem, Col, Card, Row, Icon} from 'react-materialize';
+
+import ParksContainer from './containers/ParksContainer'
 
 // TODO: NavBar, logo only correct aligned when using left
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+  this.state = {
+  parks: [
+    {
+      name: 'Wriggly Field Dog Friendly Area',
+      count: '1'
+    }
+  ]
+}
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar brand='Bark Park' left>
-          <NavItem onClick={() => console.log('test click')}>Getting started</NavItem>
+          <NavItem onClick={() => console.log('test click')}><Icon>add_circle_outline</Icon></NavItem>
         </Navbar>
+
+        <ParksContainer />
+
         <Row>
           <Col s={3}>
-            <Card title="Wriggly Field Dog Friendly Area">
-              Count: 1
-            </Card>
-        </Col>
-        <Col s={3}>
-          <Card title="Belmont Harbor Dog Beach">
-              Count: 1
-          </Card>
-        </Col>
-        <Col s={3}>
-          <Card title="Logan Square Dog Park">
-              Count: 1
+            <Card title={this.state.parks[0].name}>
+              Count: {this.state.parks[0].count}
             </Card>
         </Col>
       </Row>
@@ -35,3 +42,22 @@ class App extends Component {
 }
 
 export default App;
+
+// HARD CODED VERSION
+// <Row>
+//   <Col s={3}>
+//     <Card title="Wriggly Field Dog Friendly Area">
+//       Count: 1
+//     </Card>
+// </Col>
+// <Col s={3}>
+//   <Card title="Belmont Harbor Dog Beach">
+//       Count: 1
+//   </Card>
+// </Col>
+// <Col s={3}>
+//   <Card title="Logan Square Dog Park">
+//       Count: 1
+//     </Card>
+// </Col>
+// </Row>
---
+++ bark-park/src/containers/ParksContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class ParksContainer extends Component {
+
+  render(){
+    return(
+      <div>
+        <p>PARKS GO HERE</p>
+      </div>
+    )
+  }
+}
+
+
+export default ParksContainer
