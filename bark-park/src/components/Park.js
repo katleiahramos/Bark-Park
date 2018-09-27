6f2f99c4f3e9c13e46689e7662a2e3743db8b0b4
katleiahramos <katleiahramos@gmail.com>
2018-09-27 11:40:45 -0700

    add modal to park card

--- bark-park/src/components/Park.js
+++ bark-park/src/components/Park.js
@@ -1,27 +1,25 @@
-import { Card, Col, Button } from "react-materialize";
+import { Card, Col, Button, Modal } from "react-materialize";
 import React from "react";
 
 const Park = ({ parkInfo, deletePark }) => {
   // parkInfo =>  {name: "Wriggly Field Dog Friendly Area", address: "2645 N Sheffield Ave, Chicago, IL 60614", id: "cjmkvc3xu00023b5t4t1z4aq1", count: 1}
-  const handleOnClick = () => {
-    // want callback props here to open modal park object (parkInfo)
 
-  };
-
-  const handleDeleteClick = () => {
-    deletePark(parkInfo.id)
-  }
-
-  return (
+  const parkCard = (
     <Col s={3}>
-      <Card onClick={handleOnClick} title={parkInfo.name}>
-        <p>Address: {parkInfo.address}</p>
+      <Card title={parkInfo.name}>
         <p>Count: {parkInfo.count}</p>
-        <Button onClick={handleDeleteClick} waves="light">
-          Delete Park
-        </Button>
       </Card>
     </Col>
+  );
+
+  return (
+    <Modal trigger={parkCard} header={parkInfo.name}>
+      <p>Address: {parkInfo.address}</p>
+      <p>Count: {parkInfo.count}</p>
+      <Button onClick={() => deletePark(parkInfo.id)} waves="light">
+        Delete Park
+      </Button>
+    </Modal>
   );
 };
 
