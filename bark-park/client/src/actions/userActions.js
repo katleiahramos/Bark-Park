8d023fe5bcbd7fabf13f7411df08fab289a014c2
katleiahramos <katleiahramos@gmail.com>
2018-10-10 16:35:36 -0500

    seperate user actions and park actions

--- bark-park/client/src/actions/userActions.js
+++ bark-park/client/src/actions/userActions.js
@@ -1,16 +1,22 @@
 
 export function loginUser(loginParams) {
-    const body = JSON.stringify(loginParams)
+    
+    return (dispatch) => {
+        const body = JSON.stringify(loginParams)
+
+        dispatch({type: "BEGIN_LOGIN"})
+        
         return fetch("/api/login", {
             method: 'post',
             body: body,
             headers: { "Content-type": 'application/json' },
         })
-            .then(resp => resp.json())
-            
-    
-
-        
+            .then(resp => resp.json()) 
+            .then( user => {
+                localStorage.setItem("jwtToken", user.jwt)
+                dispatch({type: "LOGIN", payload: user})})   
+    }
+ 
 }
 
 export function logoutUser(){
