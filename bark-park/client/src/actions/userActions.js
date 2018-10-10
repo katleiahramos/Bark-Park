d975b8cbb818ddca3a0ed480f2991ef297079bf4
katleiahramos <katleiahramos@gmail.com>
2018-10-10 17:05:01 -0500

    loginUser stores username in local storage

--- bark-park/client/src/actions/userActions.js
+++ bark-park/client/src/actions/userActions.js
@@ -1,24 +1,27 @@
 
 export function loginUser(loginParams) {
-    
+
     return (dispatch) => {
         const body = JSON.stringify(loginParams)
 
-        dispatch({type: "BEGIN_LOGIN"})
-        
+        dispatch({ type: "BEGIN_LOGIN" })
+
         return fetch("/api/login", {
             method: 'post',
             body: body,
             headers: { "Content-type": 'application/json' },
         })
-            .then(resp => resp.json()) 
-            .then( user => {
-                localStorage.setItem("jwtToken", user.jwt)
-                dispatch({type: "LOGIN", payload: user})})   
+            .then(resp => resp.json())
+            .then(userInfo => {
+                localStorage.setItem("jwtToken", userInfo.jwt)
+                localStorage.setItem("currentUser", userInfo.user.username)
+                dispatch({ type: "LOGIN", payload: userInfo })
+            })
     }
- 
+
 }
 
-export function logoutUser(){
+export function logoutUser() {
     localStorage.removeItem("jwtToken")
+    localStorage.removeItem("currentUser")
 }
