620984cd55bdcdb9d0c9607f91f375372f45fb5b
katleiahramos <katleiahramos@gmail.com>
2018-10-11 16:45:40 -0500

    add relationships and attributes to serializers

--- bark-park/app/serializers/park_serializer.rb
+++ bark-park/app/serializers/park_serializer.rb
@@ -1,3 +1,5 @@
 class ParkSerializer < ActiveModel::Serializer
     attributes :id, :name, :address, :count, :lat, :long
+    has_many :users
+    has_many :checkins
 end
--- bark-park/app/serializers/user_serializer.rb
+++ bark-park/app/serializers/user_serializer.rb
@@ -1,3 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :password_digest, :admin
+  has_many :parks 
+  has_many :checkins 
 end
