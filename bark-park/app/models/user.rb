67faa8a0af67132d694cf3023d233e9930277419
katleiahramos <katleiahramos@gmail.com>
2018-10-11 17:13:29 -0500

    add current user scope method to users model

--- bark-park/app/models/user.rb
+++ bark-park/app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     has_many :checkins
     has_many :parks, through: :checkins
+
+    scope :current, -> {joins(:checkins).where('checkins.active =?', true)}
+   
 end
