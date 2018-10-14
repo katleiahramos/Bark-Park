7a2cca55c25d84c001d575449e04adafc13c7ef2
katleiahramos <katleiahramos@gmail.com>
2018-10-14 16:02:35 -0500

    add router component

---
+++ bark-park/client/src/components/appRouter.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import App from '../App';
+import Login from './Login';
+import SignupForm from './SignupForm'
+import Welcome from './Welcome';
+
+
+const AppRouter = () => {
+    return (
+        <Router>
+            <div>
+                <Route exact path="/login" component={Login}></Route>
+                <Route exact path="/signup" component={SignupForm}></Route>
+                <Route exact path="/app" component={App}></Route>
+                <Route exact path="/" component={Welcome}></Route>
+
+            </div>
+        </Router>
+    )
+}
+
+export default AppRouter
--- bark-park/client/src/index.js
+++ bark-park/client/src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import rootReducer from './reducers/index.js'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
 import thunk from 'redux-thunk';
-import Login from './components/Login';
-import SignupForm from './components/SignupForm'
-import Welcome from './components/Welcome';
+// import Login from './components/Login';
+// import SignupForm from './components/SignupForm'
+// import Welcome from './components/Welcome';
 
+import AppRouter from './components/appRouter'
 const store = createStore(rootReducer, applyMiddleware(thunk))
 
 
@@ -19,15 +20,7 @@ const store = createStore(rootReducer, applyMiddleware(thunk))
 
 ReactDOM.render((
     <Provider store={store}>
-        <Router>
-            <div>
-                <Route exact path="/login" component={Login}></Route>
-                <Route exact path="/signup" component={SignupForm}></Route>
-                <Route exact path="/app" component={App}></Route>
-                <Route exact path="/" component={Welcome}></Route>
-
-            </div>
-        </Router>
+        <AppRouter />
     </Provider>
     ), document.getElementById('root'));
 
