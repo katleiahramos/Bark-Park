2be427aa6942397741f1c75f2b4d4f8bc5876b8d
katleiahramos <katleiahramos@gmail.com>
2018-10-15 12:05:15 -0500

    add blurb to welcome page

--- bark-park/client/src/components/Welcome.js
+++ bark-park/client/src/components/Welcome.js
@@ -7,7 +7,7 @@ const Welcome = () => {
     return (
         <div className="Welcome z-depth-5 center-align">
             <h2 className="teal-text">Bark Park</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eu dictum magna. Sed pulvinar justo nisi, sit amet sodales nunc semper sed. Ut egestas libero vitae urna rhoncus pulvinar.</p><br/>
+            <p>Looking for dog parks or more dog friends? See local dog parks and who's currently checked-in! Create an accout below or sign in to an existing account.</p><br/>
             <Button node="a" href="/signup">Sign Up</Button> <Button node="a" href="/login">Log In</Button>
         </div>
     )
