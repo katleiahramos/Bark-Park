aa90d5fcb7bf06859f7cb0d8385a52462cb46ec6
katleiahramos <katleiahramos@gmail.com>
2018-10-15 12:04:22 -0500

    reformat code

--- bark-park/client/src/components/Nav.js
+++ bark-park/client/src/components/Nav.js
@@ -1,31 +1,31 @@
 import React from 'react';
-import { Navbar, NavItem, Modal, Row, Col, Button} from "react-materialize";
+import { Navbar, NavItem, Modal, Row, Col, Button } from "react-materialize";
 import ParkForm from './ParkForm'
 
 
-const Nav = ({ addPark, handleLogOut}) => {
+const Nav = ({ addPark, handleLogOut }) => {
 
-    return (
-    <Navbar className="teal" brand="Bark Park"  left>
-          
-     <NavItem onClick={handleLogOut}>Log Out</NavItem>
-    <Modal
-      header="Add Park"
-      trigger={
-        <NavItem
-          href='#!icon'
-          icon="add_circle">
-          Add Park
+  return (
+    <Navbar className="teal" brand="Bark Park" left>
+
+      <NavItem onClick={handleLogOut}>Log Out</NavItem>
+      <Modal
+        header="Add Park"
+        trigger={
+          <NavItem
+            href='#!icon'
+            icon="add_circle">
+            Add Park
       </NavItem>
-      }
-    >
-      <ParkForm addPark={addPark} />
-    </Modal>
-
-  
+        }
+      >
+        <ParkForm addPark={addPark} />
+      </Modal>
 
 
-  </Navbar>
+
+
+    </Navbar>
   )
 }
 
