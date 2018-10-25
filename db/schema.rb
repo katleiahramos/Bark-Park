2765ab4edf8dbf9d15bc0b02746a460abfcb6e3f
katleiahramos <katleiahramos@gmail.com>
2018-10-25 14:43:49 -0500

    sqllite to postgressql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_10_11_182638) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "checkins", force: :cascade do |t|
     t.integer "user_id"
     t.integer "park_id"
