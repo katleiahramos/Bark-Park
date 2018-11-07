a380fb207a05f27e3c2b7f585baaa5b8e7fad18b
katleiahramos <katleiahramos@gmail.com>
2018-11-07 11:36:15 -0600

    add id to checkins serializer

--- app/serializers/checkin_serializer.rb
+++ app/serializers/checkin_serializer.rb
@@ -1,5 +1,5 @@
 class CheckinSerializer < ActiveModel::Serializer
-    attributes :checkin_date, :active 
+    attributes :checkin_date, :active, :id
     belongs_to :user
     belongs_to :park
 end 
