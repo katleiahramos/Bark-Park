2a19c800e240eeeca9b19a96011562e2f55b28f4
Katleiah Ramos <katleiahramos@gmail.com>
2019-10-07 20:21:53 -0500

    add dotenv for environment variable handling

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import thunk from 'redux-thunk';
-import registerServiceWorker from './registerServiceWorker';
-import rootReducer from './reducers/index.js';
-import { createStore, applyMiddleware, compose } from 'redux';
-import { Provider } from 'react-redux';
-import AppRouter from './components/appRouter';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import thunk from "redux-thunk";
+import registerServiceWorker from "./registerServiceWorker";
+import rootReducer from "./reducers/index.js";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import AppRouter from "./components/appRouter";
+
+require("dotenv").config();
 
 // import App from './App';
 // import { BrowserRouter as Router, Route } from 'react-router-dom';
@@ -14,19 +16,18 @@ import AppRouter from './components/appRouter';
 // import SignupForm from './components/SignupForm'
 // import Welcome from './components/Welcome';
 
-
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
-
-ReactDOM.render((
-    <Provider store={store}>
-        <AppRouter />
-    </Provider>
-    ), document.getElementById('root'));
-
+ReactDOM.render(
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>,
+  document.getElementById("root")
+);
 
 registerServiceWorker();
-
