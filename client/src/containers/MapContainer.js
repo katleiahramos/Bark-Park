47b5ceec6c376b9a252908ecb615c0701a0ac6d7
Katleiah <35442460+katleiahramos@users.noreply.github.com>
2019-10-07 20:23:47 -0500

    Task: set up env vars (#7)

--- client/src/containers/MapContainer.js
+++ client/src/containers/MapContainer.js
@@ -1,8 +1,8 @@
-import React, { Component }from 'react'
-import { findNearByParks} from '../actions/parkActions'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import { findNearByParks } from "../actions/parkActions";
+import { connect } from "react-redux";
 // import Map from '../components/Map'
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 
 export class MapContainer extends Component {
   state = {
@@ -12,91 +12,84 @@ export class MapContainer extends Component {
     currentLocation: {
       lat: 41.936149,
       lng: -87.656576
-    },
+    }
   };
 
   onMarkerClick = (props, marker, e) =>
-  this.setState({
-    selectedPlace: props,
-    activeMarker: marker,
-    showingInfoWindow: true
-  });
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
 
-  componentDidMount(){
+  componentDidMount() {
     navigator.geolocation.getCurrentPosition(pos => {
       // TODO: Uncomment find near by parks after completing user auth
       // this.props.findNearByParks(pos)
       this.setState({
-        currentLocation: {lat: pos.coords.latitude, lng: pos.coords.longitude}
-      })
-    })
-    
+        currentLocation: { lat: pos.coords.latitude, lng: pos.coords.longitude }
+      });
+    });
   }
-
-  
-
-
-
-
-  
 
   renderMarkers = () => {
     return this.props.parks.map(park => {
       return (
-      <Marker 
-        key={park.id}
-        title={`${park.name}`}
-        name={`${park.name}`}
-        onClick={this.onMarkerClick}
-        position={{ lat: parseFloat(park.lat), lng: parseFloat(park.long)}}>
-      </Marker>);
-    })
-  }
-
-
+        <Marker
+          key={park.id}
+          title={`${park.name}`}
+          name={`${park.name}`}
+          onClick={this.onMarkerClick}
+          position={{ lat: parseFloat(park.lat), lng: parseFloat(park.long) }}
+        />
+      );
+    });
+  };
 
   render() {
     const style = {
-      width: '50%',
-      height: '100%',
-    }
+      width: "50%",
+      height: "100%"
+    };
 
-    
     return (
       <div className="Map-container">
-      <Map
-        google={this.props.google}
-        style={style}
-        center={this.state.currentLocation}
-        zoom={13}
-        onClick={this.onMapClicked}
-        className="Map"
+        <Map
+          google={this.props.google}
+          style={style}
+          center={this.state.currentLocation}
+          zoom={13}
+          onClick={this.onMapClicked}
+          className="Map"
+        >
+          {this.renderMarkers()}
 
-      >
-
-        {this.renderMarkers()}
-
-        <InfoWindow
-          marker={this.state.activeMarker}
-          visible={this.state.showingInfoWindow}>
-          <div>
-            <p>{this.state.selectedPlace.name}</p>
-          </div>
-        </InfoWindow>
-      </Map>
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}
+          >
+            <div>
+              <p>{this.state.selectedPlace.name}</p>
+            </div>
+          </InfoWindow>
+        </Map>
       </div>
     );
   }
 }
 
-
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    findNearByParks: currentLocation => dispatch(findNearByParks(currentLocation))
-  }
-}
+    findNearByParks: currentLocation =>
+      dispatch(findNearByParks(currentLocation))
+  };
+};
 
- 
-export default connect(null, mapDispatchToProps)(GoogleApiWrapper({
-  apiKey: ("AIzaSyBVugmujTPCHIqrUrOqE4hrGSxj6eWoSY0")
-})(MapContainer))
+export default connect(
+  null,
+  mapDispatchToProps
+)(
+  GoogleApiWrapper({
+    apiKey: `${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`
+  })(MapContainer)
+);
