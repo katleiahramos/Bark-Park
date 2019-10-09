8b6d44a570c15d8b4ebd2b23e8c463d75463cf2e
Katleiah Ramos <katleiahramos@gmail.com>
2019-10-08 19:52:20 -0500

    replace if statements with conditional rendering

--- client/src/components/Park.js
+++ client/src/components/Park.js
@@ -51,155 +51,60 @@ class Park extends Component {
   };
 
   render() {
-    console.log("in Park.js render");
+    const { checkedIn, parkInfo } = this.props;
 
-    if (
-      this.props.checkedIn &&
-      this.props.checkedIn.park.id === this.props.parkInfo.id
-    ) {
-      return (
-        <Col s={6}>
-          <Card title={this.props.parkInfo.name} className="teal lighten-5">
-            <p id={this.props.parkInfo.count}>
-              {" "}
-              <i class="fas fa-dog" /> {this.state.currentUsers.length}
-            </p>
-            <br />
+    return (
+      <Col s={6}>
+        <Card title={this.props.parkInfo.name} className="teal lighten-5">
+          <p id={this.props.parkInfo.count}>
+            {" "}
+            <i class="fas fa-dog" /> {this.state.currentUsers.length}
+          </p>
+          <br />
 
-            <Dropdown trigger={<Button icon="more_horiz" />}>
-              <Modal
-                trigger={<NavItem waves="light"> Info</NavItem>}
-                header={this.props.parkInfo.name}
-                s={6}
-              >
-                <h5>
-                  <i class="fas fa-map-pin" /> {this.props.parkInfo.address}
-                </h5>
-                <h5>
-                  <i class="fas fa-user-check" />{" "}
-                  {this.state.currentUsers.length}
-                </h5>
-                {/* commenting out showing user names for now */}
-                {/* {this.renderCurrentUsers()} */}
-              </Modal>
-              <NavItem
-                // floating
-                onClick={() => this.props.deletePark(this.props.parkInfo.id)}
-                waves="light"
-                // icon="delete"
-              >
-                Delete
-              </NavItem>
+          <Dropdown trigger={<Button icon="more_horiz" />}>
+            <Modal
+              trigger={<NavItem waves="light"> Info</NavItem>}
+              header={this.props.parkInfo.name}
+              s={6}
+            >
+              <h5>
+                <i class="fas fa-map-pin" /> {this.props.parkInfo.address}
+              </h5>
+              <h5>
+                <i class="fas fa-user-check" /> {this.state.currentUsers.length}
+              </h5>
+              {/* commenting out showing user names for now */}
+              {/* {this.renderCurrentUsers()} */}
+            </Modal>
+            <NavItem
+              onClick={() => this.props.deletePark(this.props.parkInfo.id)}
+              waves="light"
+            >
+              Delete
+            </NavItem>
 
-              <NavItem
-                // floating
-                onClick={() => this.props.editPark(this.props.parkInfo.id)}
-                waves="light"
-                // icon="edit"
-              >
-                Edit
-              </NavItem>
-            </Dropdown>
+            <NavItem
+              onClick={() => this.props.editPark(this.props.parkInfo.id)}
+              waves="light"
+            >
+              Edit
+            </NavItem>
+          </Dropdown>
 
-            <Button onClick={this.handleCheckOut}>CHECK OUT</Button>
-          </Card>
-        </Col>
-      );
-    } else if (this.props.checkedIn) {
-      return (
-        <Col s={6}>
-          <Card title={this.props.parkInfo.name} className="teal lighten-5">
-            <p id={this.props.parkInfo.count}>
-              {" "}
-              <i class="fas fa-dog" /> {this.state.currentUsers.length}
-            </p>
-            <br />
+          {checkedIn &&
+            checkedIn.park.id === parkInfo.id && (
+              <Button onClick={this.handleCheckOut}>CHECK OUT</Button>
+            )}
 
-            <Dropdown trigger={<Button icon="more_horiz" />}>
-              <Modal
-                trigger={<NavItem waves="light"> Info</NavItem>}
-                header={this.props.parkInfo.name}
-                s={6}
-              >
-                <h5>
-                  <i class="fas fa-map-pin" /> {this.props.parkInfo.address}
-                </h5>
-                <h5>
-                  <i class="fas fa-user-check" />{" "}
-                  {this.state.currentUsers.length}
-                </h5>
-                {/* commenting out showing user names for now */}
-                {/* {this.renderCurrentUsers()} */}
-              </Modal>
-              <NavItem
-                // floating
-                onClick={() => this.props.deletePark(this.props.parkInfo.id)}
-                waves="light"
-                // icon="delete"
-              >
-                Delete
-              </NavItem>
-
-              <NavItem
-                // floating
-                onClick={() => this.props.editPark(this.props.parkInfo.id)}
-                waves="light"
-                // icon="edit"
-              >
-                Edit
-              </NavItem>
-            </Dropdown>
-          </Card>
-        </Col>
-      );
-    } else {
-      return (
-        <Col s={6}>
-          <Card title={this.props.parkInfo.name} className="teal lighten-5">
-            <p id={this.props.parkInfo.count}>
-              {" "}
-              <i class="fas fa-dog" /> {this.state.currentUsers.length}
-            </p>
-            <br />
-
-            <Dropdown trigger={<Button icon="more_horiz" />}>
-              <Modal
-                trigger={<NavItem waves="light"> Info</NavItem>}
-                header={this.props.parkInfo.name}
-                s={6}
-              >
-                <h5>
-                  <i class="fas fa-map-pin" /> {this.props.parkInfo.address}
-                </h5>
-                <h5>
-                  <i class="fas fa-user-check" />{" "}
-                  {this.state.currentUsers.length}
-                </h5>
-                {/* commenting out showing user names for now */}
-                {/* {this.renderCurrentUsers()} */}
-              </Modal>
-              <NavItem
-                onClick={() => this.props.deletePark(this.props.parkInfo.id)}
-                waves="light"
-              >
-                Delete
-              </NavItem>
-
-              <NavItem
-                onClick={() => this.props.editPark(this.props.parkInfo.id)}
-                waves="light"
-              >
-                Edit
-              </NavItem>
-            </Dropdown>
-
+          {!checkedIn && (
             <Button onClick={this.handleCheckIn}>
               <i class="fas fa-user-check" /> Check In
             </Button>
-          </Card>
-        </Col>
-      );
-    }
+          )}
+        </Card>
+      </Col>
+    );
   }
 }
 
@@ -219,44 +124,3 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(Park);
-
-// const Park = ({ parkInfo, deletePark, editPark, checkIn, fetchCurrentUsers }) => {
-//   // parkInfo =>  {name: "Wriggly Field Dog Friendly Area", address: "2645 N Sheffield Ave, Chicago, IL 60614", id: "cjmkvc3xu00023b5t4t1z4aq1", count: 1}
-
-//   // TODO: menu should be a circle menu button user clicks on and then all options populate around button
-//   return (
-//     <Col s={6}>
-//       <Card title={parkInfo.name} className="teal lighten-5">
-//         <p><Button icon="nature_people" floating />Count: {parkInfo.count}</p>
-
-//         <Dropdown trigger={<Button floating icon="more_horiz" />}>
-//           <Modal
-//             trigger={<Button floating icon="info_outline" waves="light" />}
-//             header={parkInfo.name}
-//             s={6}
-//           >
-//             <h5>Address: {parkInfo.address}</h5>
-//             <h5>Count: {parkInfo.count}</h5>
-//           </Modal>
-//           <Button
-//             floating
-//             onClick={() => deletePark(parkInfo.id)}
-//             waves="light"
-//             icon="delete"
-//           />
-//           <Button
-//             floating
-//             onClick={() => editPark(parkInfo.id)}
-//             waves="light"
-//             icon="edit"
-//           />
-//         </Dropdown>
-//         <Button
-//           floating
-//           icon="check_circle_outline"
-//           onClick={() => checkIn(parkInfo)}
-//         />
-//       </Card>
-//     </Col>
-//   );
-// };
