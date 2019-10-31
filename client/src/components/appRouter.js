76121dbbf7d4610e640ca95c6ee516bf4ff5cc21
Oscar Delgadillo <oscar.delgadillo@mastery.net>
2019-10-30 23:27:11 -0500

    Runs prettier

--- client/src/components/appRouter.js
+++ client/src/components/appRouter.js
@@ -1,23 +1,21 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import App from '../App';
-import Login from './Login';
-import SignupForm from './SignupForm'
-import Welcome from './Welcome';
-
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import App from "../App";
+import Login from "./Login";
+import SignupForm from "./SignupForm";
+import Welcome from "./Welcome";
 
 const AppRouter = () => {
-    return (
-        <Router>
-            <div>
-                <Route exact path="/login" component={Login}></Route>
-                <Route exact path="/signup" component={SignupForm}></Route>
-                <Route exact path="/app" component={App}></Route>
-                <Route exact path="/" component={Welcome}></Route>
+  return (
+    <Router>
+      <div>
+        <Route exact path="/login" component={Login}></Route>
+        <Route exact path="/signup" component={SignupForm}></Route>
+        <Route exact path="/app" component={App}></Route>
+        <Route exact path="/" component={Welcome}></Route>
+      </div>
+    </Router>
+  );
+};
 
-            </div>
-        </Router>
-    )
-}
-
-export default AppRouter
+export default AppRouter;
