1c7444d6f86b7087e62ad9cbf11c69186289eb34
Oscar Delgadillo <oscar.delgadillo@mastery.net>
2019-11-25 19:08:41 -0600

    Fix hard refresh (#46)
    * Fix checked in state resetting on refresh.
    
    * Add env file to gitignore file

--- client/src/components/Parks.js
+++ client/src/components/Parks.js
@@ -2,42 +2,43 @@ import React from "react";
 import { Row } from "react-materialize";
 import Park from "./Park";
 
-const Parks = ({ deletePark, editPark, parks, checkIn, fetchCurrentUsers}) => {
+const Parks = ({
+  currentUser,
+  deletePark,
+  editPark,
+  parks,
+  checkIn,
+  fetchCurrentUsers
+}) => {
   const renderParks = () => {
     return parks.map((park, idx) => {
       // park => {name: "Wriggly Field Dog Friendly Area", address: "2645 N Sheffield Ave, Chicago, IL 60614", id: "cjmkvc3xu00023b5t4t1z4aq1", count: 1}
       return (
-        <Park 
+        <Park
           deletePark={deletePark}
-          editPark={editPark} 
-          parkInfo={park} 
+          editPark={editPark}
+          parkInfo={park}
           key={idx}
-          // checkIn={checkIn} 
-          fetchCurrentUsers={fetchCurrentUsers}/>
+          // checkIn={checkIn}
+          fetchCurrentUsers={fetchCurrentUsers}
+        />
       );
     });
   };
 
   const style = {
-    heigt : "100%"
-  }
+    heigth: "100%"
+  };
 
   const rowStyle = {
-    marginBottom : "0"
-  }
+    marginBottom: "0"
+  };
 
   return (
-    <div className="Parks" style={style} >
-      <Row style={rowStyle}>
-        {renderParks()} 
-      
-      </Row>
-
-      
+    <div className="Parks" style={style}>
+      <Row style={rowStyle}>{renderParks()}</Row>
     </div>
   );
-
-
-}
+};
 
 export default Parks;
